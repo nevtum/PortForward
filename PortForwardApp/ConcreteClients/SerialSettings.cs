68848d2a22d929d4f5bf5a56161682ee1456c283
Neville Tummon <nt.devs@gmail.com>
2015-05-28 22:14:40 +1000

    added serial client (yet to be tested)

---
+++ PortForwardApp/ConcreteClients/SerialSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PortForwardApp.ConcreteClients
+{
+    public class SerialSettings
+    {
+        public int BaudRate { get; set; }
+
+        public System.IO.Ports.Parity Parity { get; set; }
+
+        public int DataBits { get; set; }
+
+        public System.IO.Ports.StopBits StopBits { get; set; }
+
+        public string PortName { get; set; }
+    }
+}
---
+++ PortForwardApp/ConcreteClients/SerialTestClient.cs
@@ -0,0 +1,42 @@
+﻿using PortForward;
+using System;
+using System.IO.Ports;
+
+namespace PortForwardApp.ConcreteClients
+{
+    /// <summary>
+    /// Not tested yet
+    /// </summary>
+    public class SerialTestClient : Client, IDisposable
+    {
+        private SerialPort _serialPort;
+
+        public SerialTestClient(SerialSettings settings)
+        {
+            _serialPort = new SerialPort(settings.PortName,
+                settings.BaudRate,
+                settings.Parity,
+                settings.DataBits,
+                settings.StopBits);
+
+            _serialPort.DataReceived += OnSerialDataReceived;
+        }
+
+        public override void HandleRx(object sender, EventArgs e)
+        {
+            byte[] bytes = (byte[])sender;
+            _serialPort.Write(bytes, 0, bytes.Length);
+        }
+
+        private void OnSerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] bytes = (byte[])sender;
+            Transmit(bytes);
+        }
+
+        void IDisposable.Dispose()
+        {
+            _serialPort.DataReceived -= OnSerialDataReceived;
+        }
+    }
+}
