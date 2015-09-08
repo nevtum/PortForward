e12c6d6cae5bdc09e53ac2c90f5b0bdd673771f1
Neville Tummon <nt.devs@gmail.com>
2015-09-08 12:50:55 +1000

    added ability to log tx as well as rx
    Conflicts:
    	PortForwardApp/Program.cs

--- PortForwardApp/ConcreteClients/LoggingClient.cs
+++ PortForwardApp/ConcreteClients/LoggingClient.cs
@@ -3,8 +3,22 @@ using System.IO;
 
 namespace PortForward
 {
-    class LoggingClient : Client
+    public class LoggingClient : Client
     {
+        public override void Push(byte[] message)
+        {
+            string messageString = BitConverter.ToString(message);
+            Console.WriteLine("Tx: {0}", messageString);
+
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                string msg = string.Format("[Tx] {0}: {1}", DateTime.Now, messageString);
+                w.WriteLine(msg);
+            }
+
+            base.Push(message);
+        }
+
         public override void HandleResponse(object sender, EventArgs e)
         {
             byte[] bytes = (byte[])sender;
@@ -13,7 +27,7 @@ namespace PortForward
 
             using (StreamWriter w = File.AppendText("log.txt"))
             {
-                string msg = string.Format("{0}: {1}", DateTime.Now, message);
+                string msg = string.Format("[Rx] {0}: {1}", DateTime.Now, message);
                 w.WriteLine(msg);
             }
         }
