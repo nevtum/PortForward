dce2e6e0a5edee64eb97d9c8979294f1230e3d70
Neville Tummon <nt.devs@gmail.com>
2016-09-21 23:10:05 +1000

    wired in PRISM and got first viewmodel bound to view

---
+++ PortForwardGUI/Logging/ViewModels/LogControlViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortForwardGUI.Logging.ViewModels
+{
+    public class LogControlViewModel : BindableBase
+    {
+        public List<LoggedItem> Items
+        {
+            get
+            {
+                List<LoggedItem> items = new List<LoggedItem>();
+                items.Add(LoggedItem.IncomingData(1, new byte[] { 0x1, 0x2, 0x3 }));
+                items.Add(LoggedItem.IncomingData(2, new byte[] { 0x1, 0x2, 0x3, 0x3F }));
+                items.Add(LoggedItem.OutgoingData(3, new byte[] { 0x1, 0x2D, 0x3B, 0x8A }));
+                items.Add(LoggedItem.IncomingData(4, new byte[] { 0xE, 0xFF, 0x2A }));
+                return items;
+            }
+        }
+    }
+}
--- PortForwardGUI/Logging/Views/Master.xaml.cs
+++ PortForwardGUI/Logging/Views/Master.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using PortForwardGUI.Logging.ViewModels;
 using System.Windows.Controls;
 
 namespace PortForwardGUI.Logging.Views
@@ -11,14 +11,7 @@ namespace PortForwardGUI.Logging.Views
         public Master()
         {
             InitializeComponent();
-
-            List<LoggedItem> items = new List<LoggedItem>();
-            items.Add(LoggedItem.IncomingData(1, new byte[] { 0x1, 0x2, 0x3 }));
-            items.Add(LoggedItem.IncomingData(2, new byte[] { 0x1, 0x2, 0x3, 0x3F }));
-            items.Add(LoggedItem.OutgoingData(3, new byte[] { 0x1, 0x2D, 0x3B, 0x8A }));
-            items.Add(LoggedItem.IncomingData(4, new byte[] { 0xE, 0xFF, 0x2A }));
-
-            _myListView.ItemsSource = items;
+            DataContext = new LogControlViewModel();
         }
     }
 }
