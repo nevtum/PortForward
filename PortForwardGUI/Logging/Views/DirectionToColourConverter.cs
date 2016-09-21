a82fddc3120ab09840b3d758283e6d6ce103e948
Neville Tummon <nt.devs@gmail.com>
2016-09-21 22:56:13 +1000

    template out the views make them look a bit nicer

---
+++ PortForwardGUI/Logging/Views/DirectionToColourConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace PortForwardGUI.Logging.Views
+{
+    public class DirectionToColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DataFlow direction = (DataFlow)value;
+
+            if (direction == DataFlow.Incoming)
+                return new SolidColorBrush(Colors.Blue);
+            else if (direction == DataFlow.Outgoing)
+                return new SolidColorBrush(Colors.Green);
+
+            throw new ArgumentException("Cannot identify dataflow direction");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PortForwardGUI/Logging/Views/Master.xaml.cs
+++ PortForwardGUI/Logging/Views/Master.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
 
 namespace PortForwardGUI.Logging.Views
@@ -12,13 +12,13 @@ namespace PortForwardGUI.Logging.Views
         {
             InitializeComponent();
 
-            ObservableCollection<LoggedItem> items = new ObservableCollection<LoggedItem>();
+            List<LoggedItem> items = new List<LoggedItem>();
             items.Add(LoggedItem.IncomingData(1, new byte[] { 0x1, 0x2, 0x3 }));
-            items.Add(LoggedItem.IncomingData(2, new byte[] { 0x1, 0x2, 0x3 }));
-            items.Add(LoggedItem.OutgoingData(3, new byte[] { 0x1, 0x2, 0x3 }));
-            items.Add(LoggedItem.IncomingData(4, new byte[] { 0x1, 0x2, 0x3 }));
+            items.Add(LoggedItem.IncomingData(2, new byte[] { 0x1, 0x2, 0x3, 0x3F }));
+            items.Add(LoggedItem.OutgoingData(3, new byte[] { 0x1, 0x2D, 0x3B, 0x8A }));
+            items.Add(LoggedItem.IncomingData(4, new byte[] { 0xE, 0xFF, 0x2A }));
 
-            _mydatagrid.ItemsSource = items;
+            _myListView.ItemsSource = items;
         }
     }
 }
